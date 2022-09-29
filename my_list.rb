8e649f67e03bbc67214ee8daa1f3a31042a7bb24
ijeoma <ijeomaodinko@gmail.com>
2022-09-29 16:09:57 -0400

    Add codes for list

--- my_list.rb
+++ my_list.rb
@@ -15,3 +15,17 @@ class MyList
     @list.each { |e| block.call(e) }
   end
 end
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts(list.all? { |e| e < 5 }) # true
+puts(list.all? { |e| e > 5 }) # false
+
+# Test #any?
+puts(list.any? { |e| e == 2 }) # true
+puts(list.any? { |e| e == 5 }) # false
+
+# Test #filter
+print(list.filter(&:even?)) # [2, 4]
