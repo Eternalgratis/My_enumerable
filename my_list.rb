70e841bc80346a9f6fdd7bd3b0f79b1fa4f5b58e
Eternalgratis <tyaps002@gmail.com>
2022-09-29 20:46:57 +0100

    Add the each method

--- my_list.rb
+++ my_list.rb
@@ -5,4 +5,11 @@ class MyList
     # initialize intance variable @list
     @list = param
   end
+
+  # Method to yields (call MyEnumerable methods) successive it of @list
+  def each(&block)
+    return 'No block given' unless block_given?
+
+    @list.each { |e| block.call(e) }
+  end
 end
