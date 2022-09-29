aa788ba8f8fbba7a15eb0c0f9b626e6902d249fc
Eternalgratis <tyaps002@gmail.com>
2022-09-29 21:00:04 +0100

    Add method any

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,9 +1,17 @@
 module MyEnumerable
-    def all?
-        result = true
-        each do |elem|
-          result = false unless yield elem
-        end
-        result
-      end
+  def all?
+    result = true
+    each do |elem|
+      result = false unless yield elem
+    end
+    result
+  end
+
+  def any?
+    result = false
+    each do |elem|
+      result = true if yield elem
+    end
+    result
+  end
 end
